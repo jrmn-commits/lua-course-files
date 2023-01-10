49cd50bfd4f43b1f73ac361d145c3a74340f916b
jrmn-commits <jrmnmcntsh@gmail.com>
2023-01-10 01:10:01 -0500

    Updating activity file.

---
+++ activity_01_011023.lua
@@ -0,0 +1,16 @@
+print("Var Examples:")
+
+score = 0
+
+-- Type lives = 3 / 2 to get float ; type lives 3 // 2 to get int
+lives = 3
+
+p1_name = "Billy"
+p2_name = "Jimmy"
+
+print(type(score))
+
+print("The amount of lives Billy has is " ..lives)
+
+-- To use varname in sentence, type ..varname after quoted phrase
+print("The player that is active now is " ..p1_name)
